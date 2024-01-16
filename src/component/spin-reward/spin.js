d9682a21c2ce9d7ba91d3058138428ba939aede8
duong--dev <beabyduong@gmail.com>
2024-01-16 18:48:42 +0700

    hello new display

---
+++ src/component/spin-reward/spin.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './spin.scss';
+
+const SpinAndWin = () => {
+
+    return (
+        <div className='Spin-container'>
+
+        </div>
+    );
+};
+
+export default SpinAndWin;
--- src/router/game/game.js
+++ src/router/game/game.js
@@ -1,4 +1,5 @@
 import Nav from '../../component/Navigation/Nav';
+import SpinAndWin from '../../component/spin-reward/spin';
 import './game.scss';
 
 
@@ -6,6 +7,9 @@ function Game() {
     return (
         <div className='Game-container'>
             <Nav />
+
+            <div><h1>Spin reward</h1></div>
+            <SpinAndWin />
         </div>
     )
 };
